e8a7c7a3527a6a8eec14cb0e1c4c3bfd01aec351
Sombreroboy <83668223+Sombreroboy@users.noreply.github.com>
2021-05-19 11:54:32 -0600

    Almost complete Word-bank file
    just need to finish up display functions

--- game/word_bank.py
+++ game/word_bank.py
@@ -1,27 +1,52 @@
 import random
+
+with open("BD.txt", "r") as file:
+        allText = file.read()
+        words = list(map(str, allText.splitlines()))
+
 class Word_bank():
-  #phrases = string[]
+    def __init__(self):
+        """Class constructor. Declares and initializes instance attributes.
+
+        Args:
+            self (Rabbit): An instance of rabbit.
+    """
+        self.active_phrase = ""
+        self.distance = [0, 0]
   #active_phrase = string
 
-    #pick a random word from the bible dictionary
+    #pick a random phrase from the bible dictionary
     
     # Open the file in read mode
-    with open("BD.txt", "r") as file:
-        allText = file.read()
-        words = list(map(str, allText.split()))
+    
+        self.words = words
+        
     
         # print random string
+    
         
 
 
-    def choose_active_Phrase(words):
-        word = (random.choice(words))
-        while len(word) <=2 or len(word) >= 10:
-            word = (random.choice(words))
-        print(word)
-    """
-    def check_letter():
+    def choose_active_Phrase(self,):
+        active_phrase = (random.choice(words))
+        while len(active_phrase) <=2 or len(active_phrase) >= 15:
+            active_phrase = (random.choice(words))
+            active_phrase.replace(""," ")
+        #print("choose " +active_phrase)
+        self.active_phrase = active_phrase
+    
+    
+    def check_letter(self):
+        #from game.player import guesses
+        guesses = ['a','e','i','o','f','g']
+        for c in self.active_phrase:
+            if c in guesses:
+                print(c)
+                #so how do I implement this with display so it shows the characters in this list on the lines from display?
+                #I'm just a bit confused as to how its split vs nto just doing it all in one function
         pass
-    def display():
-        pass
-    """
+    
+    def display(self):
+        for c in self.active_phrase:
+            print("_",end = "")
+        print()
