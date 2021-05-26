378d7243fc99b9f44df02706b652f8ff55c0ca46
Seth Burton <sethburton80@gmail.com>
2021-05-26 11:15:32 -0600

    i did bug fixes

--- game/player.py
+++ game/player.py
@@ -1,12 +1,12 @@
 class Player():
     def __init__(self):
-        chosen_letters = []
+        self.chosen_letters = []
 
     def guess_letter(self):
-        guess = "da"
-        while len(guess) != 1 and guess in self.chosen_letters:
-            guess = str(input("Guess your letter a-z!"))
-        self.chosen_letters.append(guess)  
+        guess = str(input("Guess your letter a-z! ")).lower()
+        while len(guess) != 1 or guess in self.chosen_letters:
+            guess = str(input("Guess your letter a-z! "))
+        self.chosen_letters.append(guess.lower())  
         return guess
 
          
